38e793f662d77fadd54889a8ced82842599783ba
dhemsley <dhemsley@wlapcs.org>
2018-01-18 18:56:27 +0000

    added 3 sliders

--- sketch.js
+++ sketch.js
@@ -1,8 +1,27 @@
+var rusheaSlider;
+var greenSlider;
+var blueSlider;
+
+
 function setup(){
     createCanvas(600,400);
     background(0);
+    
+    //createSlider(min, max, default)
+    rusheaSlider = createSlider(0, 255, 113)
+   rusheaSlider.position(20,20); 
+   
+   greenSlider = createSlider(0, 255, 113)
+   greenSlider.position(20,50);
+   
+   blueSlider = createSlider(0, 255, 113);
+   blueSlider.position(20,80)
 }
 
 function draw(){
+    redValue = rusheaSlider.value();
+    greenValue = greenSlider.value();
+    blueValue = blueSlider.value();
     
+background(redValue, greenValue, blueValue);
 }
